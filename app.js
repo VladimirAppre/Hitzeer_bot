a2f94622f26929a7eceafa2431384e80ce8c5db0
Vladimir Karapostol <60037221+VladimirAppre@users.noreply.github.com>
2020-05-28 11:19:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1159395247:AAFF3IskRJ7LQ_C7W6p1c4BqEjgQ1cUAwyU';
+const token = '';
 
 const bot = new TelegramBot(token, {polling: true});
 
